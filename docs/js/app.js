1495c81cf2f732c3f19cef45fa37eeb591b67560
Philip Prophet <philip.prophet@gmail.com>
2017-11-26 17:10:46 +0100

    Added QR code and swish payload

--- docs/js/app.js
+++ docs/js/app.js
@@ -15,10 +15,18 @@ if (!localStorage.getItem("keys")) {
   cache.keys = JSON.parse(localStorage.getItem("keys"));
 }
 
-// Generate QR code
+// Set fake balance
 cache.balance = 0.3
 
+// Generate QR code
+new QR().toDataUrl({to: "0x" +cache.keys.address}).then(function(result) {
+  console.log(Object.keys(result))
+  document.getElementById("qr").src = result.dataURL
+})
+
 // Update user interface
-document.getElementById("account").innerHTML = cache.keys.address;
+document.getElementById("address").innerHTML = cache.keys.address;
 document.getElementById("balance_eth").innerHTML = cache.balance;
 document.getElementById("balance_sek").innerHTML = cache.balance * 3800;
+
+document.getElementById("link_swish").href = encodeURI('swish://payment?data={"version":1,"payee":{"value":"070-978 27 38","editable":false},"amount":{"value":100,"editable":false},"message":{"value":"Fisk","editable":true}}')

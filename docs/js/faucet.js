de38c366bf58debba0952472cd23ccf3f703681f
Philip Prophet <philip.prophet@gmail.com>
2017-11-29 18:41:03 +0100

    New project structure

--- docs/js/faucet.js
+++
@@ -1,7 +0,0 @@
-module.exports.receive = function(address) {
-  return fetch("https://faucet.metamask.io/", {
-    method: "POST",
-    body: address
-  })
-  .then((result) => result.text())
-}
--- docs/js/spinner.js
+++
@@ -1,9 +0,0 @@
-module.exports.start = function() {
-  console.log("Starting spinner")
-  document.getElementById('spinner').removeAttribute('hidden')
-}
-
-module.exports.stop = function() {
-  console.log("Stopping spinner")
-  document.getElementById('spinner').setAttribute('hidden', true)
-}

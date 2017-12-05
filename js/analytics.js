8d78453cbc545d03210512b19d54865b8cf58355
Philip Prophet <philip.prophet@gmail.com>
2017-12-05 19:23:32 +0100

    Working on analytics

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,5 @@
 module.exports.sendEvent = function(category, action) {
     console.log("Firing analytics event")
-    ga('send', 'event', category, action)
+    console.log(window.ga)
+    window.ga('send', 'event', category, action)
 }
